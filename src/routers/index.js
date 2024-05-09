b7dcb24d33b12d8cd3df2e87cd79253071540cc5
Nikhil Aggarwal <nikhil.aggarwal1@rivigo.com>
2024-05-09 16:59:23 +0530

    Minor refactoring

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const {getProfile} = require("../middleware/getProfile");
 const router = express.Router();
 
-router.use('/contracts', getProfile, require('./contracts.router'));
-router.use('/jobs', getProfile, require('./jobs.router'));
+router.use('/contracts', getProfile, require('./contract.router'));
+router.use('/jobs', getProfile, require('./job.router'));
 router.use('/admin', require('./admin.router'));
-router.use('/balances', getProfile, require('./balances.router'));
+router.use('/balances', require('./balances.router'));
 
 
 module.exports = router;
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
-module.exports.contractService = require('./contracts.service');
-module.exports.jobService = require('./jobs.service');
+module.exports.contractService = require('./contract.service');
+module.exports.jobService = require('./job.service');
 module.exports.adminService = require('./admin.service');
 module.exports.clientService = require('./client.service');
